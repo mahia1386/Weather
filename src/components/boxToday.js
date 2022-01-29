55a1a0ecbce0fd86cde165812591b37ea7c4116b
Mahia Tavakkoli <mtg1386@outlook.com>
2022-01-29 18:06:06 +0330

    Correction of the second part (today's weather)

--- src/components/boxToday.js
+++ src/components/boxToday.js
@@ -4,7 +4,15 @@ import style from '../Font/style.css';
 
 function BoxToday(){
 
+    const [data, setData] = useState({ data: "" });
     const [tempToday, settempToday] = useState({ data: "" });
+
+    async function getData() {
+        let data = await fetch('https://api.weatherbit.io/v2.0/forecast/daily?city=Berlin&country=DE&key=e18186b3a3324db780a0f350eeba7098');
+        data = await data.json()
+        setData(data);
+    }
+
 
     async function getTemp() {
         let tempToday = await fetch('https://api.weatherbit.io/v2.0/current?city=Berlin&country=DE&key=e18186b3a3324db780a0f350eeba7098');
@@ -13,6 +21,7 @@ function BoxToday(){
     }
 
     useEffect(() => {
+        getData();
         getTemp();
     }, [])
 
@@ -50,51 +59,64 @@ function BoxToday(){
                 <div className="col col-6 box-2-2">
                     <span>
                         رطوبت:
-                            {tempToday.data
-                            ? <span className="span-from-1-2"> {Math.trunc(tempToday.data[0].rh)} </span>
+                            {data.data
+                            ? <span className="span-from-1-2"> {Math.trunc(data.data[0].rh)} %</span>
                             : null}
                     </span>
                     <br />
                     <hr />
                     <span>
                         فشار:
-                            {tempToday.data
-                            ? <span className="span-from-1-2"> {Math.trunc(tempToday.data[0].pres)} </span>
+                            {data.data
+                            ? <span className="span-from-1-2"> {Math.trunc(data.data[0].pres)} </span>
                             : null}
                     </span>
                     <br />
                     <hr />
                     <span>
                         قابلیت دید:
-                            {tempToday.data
-                            ? <span className="span-from-1-2"> {Math.trunc(tempToday.data[0].vis)} </span>
+                            {data.data
+                            ? <span className="span-from-1-2"> {Math.trunc(data.data[0].vis)} </span>
+                            : null}
+                    </span>
+                    <br />
+                    <hr />
+                    <span>
+                        احتمال بارش :
+                            {data.data
+                            ? <span className="span-from-1-2"> {Math.trunc(data.data[0].pop)} %</span>
                             : null}
                     </span>
                 </div>
                 <div className="col col-6 box-2-2">
                     <span>
                         باد:
-                            {tempToday.data
-                            ? <span className="span-from-1-2"> {Math.trunc(tempToday.data[0].wind_spd)} </span>
-                            : null}
-                        {tempToday.data
-                            ? <span className="span-from-1-2"> {Math.trunc(tempToday.data[0].wind_cdir)} </span>
+                            {data.data
+                            ? <span className="span-from-1-2"> {Math.round(data.data[0].wind_spd)} کیلومتر/ساعت</span>
                             : null}
                     </span>
                     <br />
                     <hr />
                     <span>
                         نقطه شبنم:
-                            {tempToday.data
-                            ? <span className="span-from-1-2"> {Math.trunc(tempToday.data[0].dewpt)} </span>
+                            {data.data
+                            ? <span className="span-from-1-2"> {Math.trunc(data.data[0].dewpt)} </span>
                             : null}
                     </span>
                     <br />
                     <hr />
                     <span>
                         شاخص فرابنفش:
-                        {tempToday.data
-                            ? <span className="span-from-1-2"> {Math.trunc(tempToday.data[0].uv)} </span>
+                        {data.data
+                            ? <span className="span-from-1-2"> {Math.trunc(data.data[0].uv)} </span>
+                            : null}
+                    </span>
+                    <br />
+                    <hr />
+                    <span>
+                        جهت باد:
+                       {data.data
+                            ? <span className="span-from-1-2"> {data.data[0].wind_cdir} </span>
                             : null}
                     </span>
                 </div>
